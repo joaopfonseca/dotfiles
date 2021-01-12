a9ca46375ad8ec90e89c3c05dddc988d85900077
joaopfonseca <jpmrfonseca@gmail.com>
2021-01-12 22:48:17 +0000

    Added zshrc

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,8 +1,10 @@
 # remove dotfiles
 rm ~/.vimrc
 rm ~/.bashrc
+rm ~/.zshrc
 
 # create symlinks
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
